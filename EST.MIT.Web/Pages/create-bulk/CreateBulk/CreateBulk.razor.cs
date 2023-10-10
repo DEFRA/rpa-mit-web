ddedbfb51b35cc1d460577cf53b092ef4eaac41f
Henry Adetunji <Henry.Adetunji@defra.gov.uk>
2023-10-10 16:05:14 +0100

    Create Upload  Bulk invoice page

---
+++ EST.MIT.Web/Pages/create-bulk/CreateBulk/CreateBulk.razor.cs
@@ -0,0 +1,24 @@
+﻿using Entities;
+using EST.MIT.Web.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace EST.MIT.Web.Pages.create_bulk.CreateBulk
+{
+    public partial class CreateBulk 
+    {
+        [Inject] private NavigationManager _nav { get; set; }
+        [Inject] private IInvoiceStateContainer _invoiceStateContainer { get; set; }
+
+        private void Start()
+        {
+            _invoiceStateContainer.SetValue(new Invoice());
+            _nav.NavigateTo("/create-invoice/account");
+        }
+
+        private void Cancel()
+        {
+            _invoiceStateContainer.SetValue(null);
+            _nav.NavigateTo("/");
+        }
+    }
+}
