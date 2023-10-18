70b6c15b5c4e7d1fbbbc24b5cc0c67881c9f116c
Henry Adetunji <Henry.Adetunji@defra.gov.uk>
2023-10-18 08:29:12 +0100

    updated the UploadFileSummary

--- EST.MIT.Web/Entities/UploadFileSummary.cs
+++ EST.MIT.Web/Entities/UploadFileSummary.cs
@@ -9,6 +9,11 @@ public class UploadFileSummary
     public string? FileType { get; set; }
     public DateTimeOffset Timestamp { get; set; }
     public string ConfirmationNumber { get; }
+    public string AccountType { get; set; } 
+    public string Organisation { get; set; }    
+    public string SchemeType { get; set; }
+    public string PaymentType { get; set; } 
+    public string UserID { get; set; }  
 
     public UploadFileSummary(string confirmationNumber)
     {
