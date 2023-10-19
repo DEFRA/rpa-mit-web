aebfa848dab088fd27ec117912a1962951d88a12
Henry Adetunji <Henry.Adetunji@defra.gov.uk>
2023-10-19 14:33:59 +0100

    unit test code coverage

--- EST.MIT.Web.Test/Pages/create-bulk/OrganisationMetaSelectionTests.cs
+++ EST.MIT.Web.Test/Pages/create-bulk/OrganisationMetaSelectionTests.cs
@@ -36,44 +36,43 @@ public class OrganisationMetaSelectionPageBulkTests : TestContext
         navigationManager?.Uri.Should().Be("http://localhost/create-bulk");
     }
 
-    // [Fact]
-    // public void No_Selection_Fails_Validation()
-    // {
-    //     _mockPageServices.Setup(x => x.Validation(It.IsAny<OrganisationSelect>(), out It.Ref<bool>.IsAny, out It.Ref<Dictionary<string, string>>.IsAny))
-    //         .Callback((object organisationSelect, out bool IsErrored, out Dictionary<string, string> errors) =>
-    //         {
-    //             IsErrored = true;
-    //             errors = new()
-    //             {
-    //                 { "Name", "Please select an organisation" }
-    //             };
-    //         });
+    [Fact]
+    public void No_Selection_Fails_Validation()
+    {
+        _mockPageServices.Setup(x => x.Validation(It.IsAny<OrganisationSelect>(), out It.Ref<bool>.IsAny, out It.Ref<Dictionary<string, List<string>>>.IsAny))
+            .Callback((object organisationSelect, out bool IsErrored, out Dictionary<string, List<string>> errors) =>
+            {
+                IsErrored = true;
+                errors = new()
+                {
+                    { "organisation", new List<string>() { "Please select an organisation" } }
+                };
+            });
 
-    //     _mockReferenceDataAPI.Setup(x => x.GetOrganisationsAsync(It.IsAny<string>()))
-    //         .Returns(Task.FromResult<ApiResponse<IEnumerable<Organisation>>>(new ApiResponse<IEnumerable<Organisation>>(HttpStatusCode.OK)
-    //         {
-    //             Data = new List<Organisation>
-    //             {
-    //                 new Organisation { code = "RPA", description = "Rural Payments Agency" }
-    //             }
-    //         }));
+        _mockReferenceDataAPI.Setup(x => x.GetOrganisationsAsync(It.IsAny<string>()))
+            .Returns(Task.FromResult<ApiResponse<IEnumerable<Organisation>>>(new ApiResponse<IEnumerable<Organisation>>(HttpStatusCode.OK)
+            {
+                Data = new List<Organisation>
+                {
+                     new Organisation { code = "RPA", description = "Rural Payments Agency" }
+                }
+            }));
 
-    //     _mockInvoiceStateContainer.SetupGet(x => x.Value).Returns(new Invoice());
+        _mockInvoiceStateContainer.SetupGet(x => x.Value).Returns(new Invoice());
 
-    //     var component = RenderComponent<OrganisationMetaSelection>();
-    //     component.FindAll("button")[0].Click();
+        var component = RenderComponent<OrganisationMetaSelection>();
+        component.FindAll("button")[0].Click();
 
-    //     component.WaitForElements("p.govuk-error-message");
+        component.WaitForElements("p.govuk-error-message");
 
-    //     var errorMessages = component.FindAll("p.govuk-error-message");
+        var errorMessages = component.FindAll("p.govuk-error-message");
 
-    //     var validation = Services.GetService<IPageServices>();
+        var validation = Services.GetService<IPageServices>();
 
-    //     errorMessages.Should().NotBeEmpty();
-    //     errorMessages.Should().HaveCount(1);
-    //     errorMessages[0].TextContent.Should().Be("Error:Please select an organisation");
-
-    // }
+        errorMessages.Should().NotBeEmpty();
+        errorMessages.Should().HaveCount(1);
+        errorMessages[0].TextContent.Should().Be("Error:Please select an organisation");
+    }
 
     [Fact]
     public void Shows_Organisation_RadioButtons()
