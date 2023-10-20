34f90e6bb75d79ade08e9bb3615a64cf7488ab80
Henry Adetunji <Henry.Adetunji@defra.gov.uk>
2023-10-20 10:38:34 +0100

    Created a class for the route fields

---
+++ EST.MIT.Web/Entities/RouteFields.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities;
+
+public class RouteFields
+{
+    public string SchemeType { get; set; }
+    public string Organisation { get; set; }
+    public string PaymentType { get; set; }
+    public string AccountType { get; set; }
+    public string UserID { get; set; }
+}
