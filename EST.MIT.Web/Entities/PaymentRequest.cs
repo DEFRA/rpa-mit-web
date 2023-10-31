8092ef11e4ec5f975771d550ce881d4da83adf23
fshehzad-defra <faisal.shehzad@defra.gov.uk>
2023-10-31 09:40:12 +0000

    207611, Add in initial notes

--- EST.MIT.Web/Entities/PaymentRequest.cs
+++ EST.MIT.Web/Entities/PaymentRequest.cs
@@ -3,34 +3,44 @@ using EST.MIT.Web.Helpers;
 using System.ComponentModel.DataAnnotations;
 using EST.MIT.Web.Attributes;
 
-namespace EST.MIT.Web.Entities;
+namespace Entities;
 
 public class PaymentRequest : Validatable
 {
     public string PaymentRequestId { get; set; }
+
     [Required]
     public string SourceSystem { get; set; } = "Manual";
+
+    // fxs needs to be updated to allow for 10 digit FRN to a long
     [Required]
     [RegularExpression("(\\d{10})", ErrorMessage = "The FRN must be 10 digits")]
     public int FRN { get; set; }
+
     [Range(2014, int.MaxValue, ErrorMessage = "The Marketing Year must be after than 2014")]
     public int MarketingYear { get; set; }
+
     [Required]
     [Range(1, int.MaxValue, ErrorMessage = "The Payment Request Number must be greater than 0")]
     public int PaymentRequestNumber { get; set; }
+
     //Invoice Number
     [Required(ErrorMessage = "The Agreement Number is required")]
     public string AgreementNumber { get; set; } = string.Empty;
+
     [Required]
     [RegularExpression("GBP|EUR", ErrorMessage = "The Currency must be either GBP or EUR")]
     public string Currency { get; set; } = "GBP";
+
     public string DueDate { get; set; } = string.Empty;
+
     [Required]
     [RegularExpression("(^(0|\\d+\\.\\d{2})$)", ErrorMessage = "The Value must be in the format 0.00")]
     public double Value { get; set; } = 0.00;
+
     public List<InvoiceLine> InvoiceLines { get; set; } = new List<InvoiceLine>();
 
-    public string AccountType { get; set; } = string.Empty;
+    public string AccountType { get; set; } = string.Empty; // extra to dto
 
     [RequiredIfAR]
     [DisplayName("Original Claim Reference")]
@@ -47,6 +57,18 @@ public class PaymentRequest : Validatable
     [RequiredIfAR]
     [DisplayName("Correction Reference - Previous AR Invoice ID")]
     public string InvoiceCorrectionReference { get; set; } = string.Empty;
+
+    //Missing below
+    // FXS add
+    //[JsonProperty("sbi")]
+    //public int SBI { get; init; } = default!;
+
+    //[JsonProperty("vendor")]
+    //public string Vendor { get; init; } = default!;
+
+    //[JsonProperty("description")]
+    //public string Description { get; init; } = default!;
+
     public override Dictionary<string, List<string>> AddErrors(Dictionary<string, List<string>> errors)
     {
         for (int invoiceLineIndex = 0; invoiceLineIndex < InvoiceLines.Count; invoiceLineIndex++)
