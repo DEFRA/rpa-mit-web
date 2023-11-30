7cfadbe5c952ea242be6bfd0d3db290b4728d4f6
Henry Adetunji <Henry.Adetunji@defra.gov.uk>
2023-11-30 15:48:31 +0000

    codebase changes

--- EST.MIT.Web/Services/InvoiceAPI.cs
+++ EST.MIT.Web/Services/InvoiceAPI.cs
@@ -325,7 +325,7 @@ public class InvoiceAPI : IInvoiceAPI
             try
             {
                 var dtoList = await response.Content.ReadFromJsonAsync<IEnumerable<PaymentRequestsBatchDTO>>();
-                return _autoMapper.Map<IEnumerable<Invoice>>(dtoList);
+                return _autoMapper.Map<IEnumerable<Invoice>>(dtoList).Where(x => x.PaymentRequests.Any());
             }
             catch (Exception ex)
             {
