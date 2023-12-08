d9a2caa3b13f10e2184c1c539c830ff1a0a25cdb
JasmeetKaur-Defra <jasmeet.kaur@defra.gov.uk>
2023-12-08 16:53:05 +0000

    Refactored status tag

--- EST.MIT.Web/Entities/ImportRequest.cs
+++ EST.MIT.Web/Entities/ImportRequest.cs
@@ -1,5 +1,6 @@
 namespace EST.MIT.Web.Entities;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 public class ImportRequest
 {
@@ -8,7 +9,6 @@ public class ImportRequest
     public long FileSize { get; set; }
     public string FileType { get; set; }
     public DateTimeOffset? Timestamp { get; set; }
-
     public string PaymentType { get; set; }
     public string Organisation { get; set; }
     public string SchemeType { get; set; }
@@ -18,6 +18,20 @@ public class ImportRequest
     public string BlobFileName { get; set; }
     public string BlobFolder { get; set; }
     public string Email { get; set; }
+
+    [JsonIgnore]
+    public string StatusTag => GetStatusTag();
+    private string GetStatusTag()
+    {
+        return (Status) switch
+        {
+            UploadStatus.Upload_successful => "govuk-tag--blue",
+            UploadStatus.Upload_failed => "govuk-tag--red",
+            UploadStatus.Upload_validated => "govuk-tag--yellow",
+            UploadStatus.Uploaded => "govuk-tag--green",
+            _ => null
+        };
+    }
 }
 
 public static class ImportRequestExtentions
--- EST.MIT.Web/Entities/UploadStatus.cs
+++ EST.MIT.Web/Entities/UploadStatus.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace EST.MIT.Web.Entities;
 
