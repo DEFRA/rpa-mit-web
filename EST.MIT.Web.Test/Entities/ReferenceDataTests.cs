af55aac460759b7f4fc7377b75c01db66b350ff1
JasmeetKaur-Defra <jasmeet.kaur@defra.gov.uk>
2023-12-11 22:09:59 +0000

    Added unit tests

---
+++ EST.MIT.Web.Test/Entities/ReferenceDataTests.cs
@@ -0,0 +1,71 @@
+﻿using EST.MIT.Web.Entities;
+
+namespace EST.MIT.Web.Test.Entities;
+
+public class ReferenceDataTests
+{
+    [Fact]
+    public void TestMainAccountProperties()
+    {
+        var mainAccount = new MainAccount
+        {
+            code = "AZ",
+            description = "main account description",
+        };
+
+        Assert.Equal("AZ", mainAccount.code);
+        Assert.Equal("main account description", mainAccount.description);
+    }
+
+    [Fact]
+    public void TestDeliveryBodyProperties()
+    {
+        var deliveryBody = new DeliveryBody
+        {
+            code = "AX",
+            description = "delivery body description",
+        };
+
+        Assert.Equal("AX", deliveryBody.code);
+        Assert.Equal("delivery body description", deliveryBody.description);
+    }
+
+    [Fact]
+    public void TestFundCodeProperties()
+    {
+        var fundCode = new FundCode
+        {
+            code = "RPA",
+            description = "fund code description",
+        };
+
+        Assert.Equal("RPA", fundCode.code);
+        Assert.Equal("fund code description", fundCode.description);
+    }
+
+    [Fact]
+    public void TestMarketingYearProperties()
+    {
+        var marketingYear = new MarketingYear
+        {
+            code = "2023",
+            description = "marketing year description",
+        };
+
+        Assert.Equal("2023", marketingYear.code);
+        Assert.Equal("marketing year description", marketingYear.description);
+    }
+
+    [Fact]
+    public void TestSchemeCodeProperties()
+    {
+        var schemeCode = new SchemeCode
+        {
+            code = "AZ",
+            description = "marketing year description",
+        };
+
+        Assert.Equal("AZ", schemeCode.code);
+        Assert.Equal("marketing year description", schemeCode.description);
+    }
+}
--- EST.MIT.Web/Entities/ReferenceData.cs
+++ EST.MIT.Web/Entities/ReferenceData.cs
@@ -1,56 +1,47 @@
-using System.Diagnostics.CodeAnalysis;
 namespace EST.MIT.Web.Entities;
 
-[ExcludeFromCodeCoverage]
 public class Organisation
 {
     public string code { get; set; }
     public string description { get; set; }
 }
 
-[ExcludeFromCodeCoverage]
 public class SchemeType
 {
     public string code { get; set; }
     public string description { get; set; }
 }
 
-[ExcludeFromCodeCoverage]
 public class PaymentType
 {
     public string code { get; set; }
     public string description { get; set; }
 }
 
-[ExcludeFromCodeCoverage]
 public class MainAccount
 {
     public string code { get; set; }
     public string description { get; set; }
 }
 
-[ExcludeFromCodeCoverage]
 public class DeliveryBody
 {
     public string code { get; set; }
     public string description { get; set; }
 }
 
-[ExcludeFromCodeCoverage]
 public class FundCode
 {
     public string code { get; set; }
     public string description { get; set; }
 }
 
-[ExcludeFromCodeCoverage]
 public class MarketingYear
 {
     public string code { get; set; }
     public string description { get; set; }
 }
 
-[ExcludeFromCodeCoverage]
 public class SchemeCode
 {
     public string code { get; set; }
