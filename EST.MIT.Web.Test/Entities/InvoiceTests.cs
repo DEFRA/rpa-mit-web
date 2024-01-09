59a5b0fb9dfe23e5f16f4cb29fb684e26eee8f2f
Henry Adetunji <Henry.Adetunji@defra.gov.uk>
2024-01-09 16:16:47 +0000

    unit tests code coverage

--- EST.MIT.Web.Test/Entities/InvoiceTests.cs
+++ EST.MIT.Web.Test/Entities/InvoiceTests.cs
@@ -222,6 +222,118 @@ public class InvoiceTests
         Assert.Equal(4, invoice.NumberOfPaymentRequests);
     }
 
+    [Fact]
+    public void When_Invoice_Has_No_Invoicelines_Then_Invoice_Cannot_Be_Approved()
+    {
+        //Arrange
+        var invoice = new Invoice()
+        {
+            PaymentRequests = new List<PaymentRequest>()
+            {
+                new PaymentRequest()
+                {
+                    FRN = "1234567890",
+                    SourceSystem = "",
+                    MarketingYear = "0",
+                    PaymentRequestNumber = 0,
+                    AgreementNumber = "",
+                    Value = 0,
+                    DueDate = "",
+                    Currency = "GBP"
+                }
+            }
+        };
+
+        //Assert
+        Assert.Equal(false, invoice.CanBeApproved);
+    }
+
+    [Fact]
+    public void When_Invoice_Has_At_Least_One_InvoiceLine_With_Total_Value_NotEqualTo_Zero_Then_Invoice_Can_Be_Approved()
+    {
+        //Arrange
+        var invoice = new Invoice()
+        {
+            PaymentRequests = new List<PaymentRequest>()
+            {
+                new PaymentRequest()
+                {
+                    FRN = "1234567890",
+                    SourceSystem = "",
+                    MarketingYear = "0",
+                    PaymentRequestNumber = 0,
+                    AgreementNumber = "",
+                    Value = 0,
+                    DueDate = "",
+                    Currency = "GBP",
+                    InvoiceLines = new List<InvoiceLine>()
+                    {
+                        new InvoiceLine()
+                        {
+                             Value = 34.89M
+                        },
+                        new InvoiceLine()
+                        {
+                            Value= 23.90M
+                        }
+                    }
+                }
+            }
+        };
+
+        //Assert
+        Assert.Equal(true, invoice.CanBeApproved);
+    }
+
+    [Fact]
+    public void When_Invoice_Has_PaymentRequest_With_No_InvoiceLines_Then_Invoice_Cannot_Be_Approved()
+    {
+        //Arrange
+        var invoice = new Invoice()
+        {
+            PaymentRequests = new List<PaymentRequest>()
+            {
+                new PaymentRequest()
+                {
+                    FRN = "1234567890",
+                    SourceSystem = "",
+                    MarketingYear = "0",
+                    PaymentRequestNumber = 0,
+                    AgreementNumber = "",
+                    Value = 0,
+                    DueDate = "",
+                    Currency = "GBP",
+                    InvoiceLines = new List<InvoiceLine>()
+                    {
+                        new InvoiceLine()
+                        {
+                             Value = 34.89M
+                        },
+                        new InvoiceLine()
+                        {
+                            Value= 23.90M
+                        }
+                    }
+                },
+
+                new PaymentRequest()
+                {
+                    FRN = "1234567890",
+                    SourceSystem = "",
+                    MarketingYear = "0",
+                    PaymentRequestNumber = 0,
+                    AgreementNumber = "",
+                    Value = 0,
+                    DueDate = "",
+                    Currency = "GBP"
+                }
+            }
+        };
+
+        //Assert
+        Assert.Equal(false, invoice.CanBeApproved);
+    }
+
     private static System.Collections.Generic.IEnumerable<ValidationResult> ValidateModel(object model)
     {
         var validationResults = new System.Collections.Generic.List<ValidationResult>();
